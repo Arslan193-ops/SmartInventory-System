395f76a0b69065e98dfbd71f3f01786f9c6cd99f
Arslan <arslanmian604@gmail.com>
2025-09-08 18:38:13 +0500

    Identity setuped in program.cs file

--- SmartInventory System/Program.cs
+++ SmartInventory System/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SmartInventory_System.Data;
 using SmartInventory_System.Services;
@@ -7,22 +8,34 @@ using SmartInventory_System.Services.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
 // 1. Register services (DI)
-builder.Services.AddControllers(); // API controllers only
-builder.Services.AddEndpointsApiExplorer(); // For minimal APIs & Swagger
-builder.Services.AddSwaggerGen(); // Swagger/OpenAPI support
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // 2. Configure EF Core DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("dbcs")));
 
+// 3. Register custom services
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IStockMovementService, StockMovementService>();
 builder.Services.AddScoped<ILowStockAlertService, LowStockAlertService>();
 
+// 4. Configure Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()   // ? link Identity to EF
+.AddDefaultTokenProviders();                        // ? needed for password reset, etc.
 
 var app = builder.Build();
 
-// 3. Configure middleware (HTTP pipeline)
+// 5. Middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -31,8 +44,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// ? authentication first, then authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers(); // Map all API controllers
+app.MapControllers();
 
 app.Run();
